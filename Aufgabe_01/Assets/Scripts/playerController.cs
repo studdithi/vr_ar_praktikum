e106cc209e10d8783d326a40c8001178a58801a8
studdithi <102218163+studdithi@users.noreply.github.com>
2022-03-23 11:24:45 +0100

    Finished with jumping

--- Aufgabe_01/Assets/Scripts/playerController.cs
+++ Aufgabe_01/Assets/Scripts/playerController.cs
@@ -6,6 +6,7 @@ public class playerController : MonoBehaviour
 {
 
     public float speed = 10f;
+    public float gravity = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +22,16 @@ public class playerController : MonoBehaviour
     void FixedUpdate() {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 1f, moveVertical);
 
         Rigidbody rb = GetComponent<Rigidbody>();
+
         rb.AddForce(movement * speed);
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(Vector3.up * 10f, ForceMode.Impulse);
+        }
+        rb.AddForce(new Vector3(0, -gravity, 0));
+
 
     }
 
