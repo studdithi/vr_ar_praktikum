7378b553babf4a706a1d3b973b62e7979d36702c
Marco Wenger <81690202+maw5979@users.noreply.github.com>
2022-03-30 11:37:08 +0200

    Aufgabe_02

---
+++ Aufgabe_02/Assets/ShowDescription.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowDescription : MonoBehaviour
+{
+    public TextMesh textMesh;
+    public string buildingName;
+
+    public void ShowName(){
+        textMesh.text = buildingName;
+        Debug.Log("ShowName");
+    }
+    public void HideName(){
+        textMesh.text = "";
+        Debug.Log("Hide Name");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
